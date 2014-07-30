a3d0b5ad96d32ad2029bb799b94290505af613f3
Ben Meier <benmeier42@gmail.com>
2014-07-30 21:48:08 +0200

    finished hough transform / filter thing

--- src/org/uct/cs/hough/CliDriver.java
+++ src/org/uct/cs/hough/CliDriver.java
@@ -2,11 +2,10 @@ package org.uct.cs.hough;
 
 import org.uct.cs.hough.display.PopUp;
 import org.uct.cs.hough.reader.ImageLoader;
-import org.uct.cs.hough.stages.Greyscale;
-import org.uct.cs.hough.stages.NormalizeStage;
-import org.uct.cs.hough.stages.SobelEdgeDetectStage;
-import org.uct.cs.hough.stages.ThresholdStage;
+import org.uct.cs.hough.stages.*;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class CliDriver
@@ -15,18 +14,27 @@ public class CliDriver
     {
         try
         {
-            PopUp.Show(
-                new ThresholdStage(210).flow(
+
+            BufferedImage original = ImageLoader.Load("samples/testseq100000.gif");
+
+            PopUp.Show(original);
+
+            ShortImageBuffer edges = new ThresholdStage(210).flow(
                     new NormalizeStage().flow(
-                        new SobelEdgeDetectStage().flow(
-                            new NormalizeStage().flow(
-                                Greyscale.Convert(
-                                    ImageLoader.Load("samples/testseq100000.gif"),
-                                    new Greyscale.FormulaAverage()
-                                )
+                            new SobelEdgeDetectStage().flow(
+                                    new NormalizeStage().flow(
+                                            Greyscale.Convert(
+                                                    original,
+                                                    new Greyscale.FormulaAverage()
+                                            )
+                                    )
                             )
-                        )
                     )
+            );
+
+            PopUp.Show(
+                new HoughFilterStage(5, 50).flow(
+                    edges
                 ).toImage()
             );
         }
--- src/org/uct/cs/hough/stages/HoughFilterStage.java
+++ src/org/uct/cs/hough/stages/HoughFilterStage.java
@@ -1,23 +1,148 @@
 package org.uct.cs.hough.stages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HoughFilterStage implements IStage
 {
     private final int minCircleRadius;
     private final int maxCircleRadius;
+    private ArrayList<IntPoint>[] circlePoints;
 
     public HoughFilterStage(int minCircleRadius, int maxCircleRadius)
     {
         this.minCircleRadius = minCircleRadius;
         this.maxCircleRadius = maxCircleRadius;
-    }
+        int numsizes = maxCircleRadius - minCircleRadius;
+        this.circlePoints = new ArrayList[(maxCircleRadius-minCircleRadius)];
+        for(int r=0;r<numsizes;r++)
+        {
+            this.circlePoints[r] = new ArrayList<>();
+            int x = minCircleRadius + r, y = 0;
+            int radiusError = 1-x;
 
-    // TODO aggressive caching of sqrt values
+            while(x >= y)
+            {
+                this.circlePoints[r].add(new IntPoint(x, y));
+                y++;
+                if (radiusError<0)
+                {
+                    radiusError += 2 * y + 1;
+                }
+                else
+                {
+                    x--;
+                    radiusError += 2 * (y - x + 1);
+                }
+            }
+        }
+    }
 
     @Override
     public ShortImageBuffer flow(ShortImageBuffer before)
     {
-        int[][][] houghSpace = new int[before.getHeight()][before.getWidth()][this.maxCircleRadius - this.minCircleRadius];
+        HoughSpace space = new HoughSpace(before.getHeight(), before.getWidth(),this.minCircleRadius, this.maxCircleRadius, false);
 
-        return null;
+        for(int y=0;y<before.getHeight();y++)
+        {
+            for(int x=0;x<before.getWidth();x++)
+            {
+                int v = before.get(y, x) & 0xFF;
+                if (v > 0)
+                {
+                    int radii = this.circlePoints.length;
+                    for(int r=0;r<radii;r++)
+                    {
+                        for(IntPoint p : this.circlePoints[r])
+                        {
+                            space.inc(y+p.y,x+p.x,r);
+                            space.inc(y+p.x,x+p.y,r);
+                            space.inc(y+p.y,x-p.x,r);
+                            space.inc(y+p.x,x-p.y,r);
+                            space.inc(y-p.y,x-p.x,r);
+                            space.inc(y-p.x,x-p.y,r);
+                            space.inc(y-p.y,x+p.x,r);
+                            space.inc(y-p.x,x+p.y,r);
+                        }
+                    }
+                }
+            }
+        }
+
+        ShortImageBuffer after = before.copyShape();
+        for(int y=0;y<before.getHeight();y++)
+        {
+            for (int x = 0; x < before.getWidth(); x++)
+            {
+                int v = (int)((space.getMax(y,x) / space.getBestMax()) * 255);
+                after.set(y,x,(short)v);
+            }
+        }
+
+        return after;
+    }
+
+    private class HoughSpace
+    {
+        private int[][][] data;
+        private float[][] maximums;
+        private int[][] maxradii;
+        private int width, height, minRadii;
+        private float highestMaximum;
+        private boolean normaliseWithRadii;
+
+        public HoughSpace(int height, int width, int minRadii, int maxRadii, boolean normaliseWithRadii)
+        {
+            this.height = height;
+            this.width = width;
+            this.minRadii = minRadii;
+            this.data = new int[height][width][maxRadii - minRadii];
+            this.maximums = new float[height][width];
+            this.maxradii = new int[height][width];
+            this.normaliseWithRadii = normaliseWithRadii;
+        }
+
+        public void inc(int y, int x, int r)
+        {
+            if (y < 0 || y >= this.height) return;
+            if (x < 0 || x >= this.width) return;
+
+            float nv = ++data[y][x][r];
+            if (normaliseWithRadii) nv /= (Math.PI * 2 * (minRadii + r));
+            if(nv > maximums[y][x])
+            {
+                maximums[y][x] = nv;
+                maxradii[y][x] = this.minRadii + r;
+
+                if(nv > highestMaximum) highestMaximum = nv;
+            }
+        }
+
+        public float getMax(int y, int x)
+        {
+            return this.maximums[y][x];
+        }
+
+        public int getMaxRadii(int y, int x)
+        {
+            return this.maxradii[y][x];
+        }
+
+        public float getBestMax()
+        {
+            return this.highestMaximum;
+        }
+    }
+
+    private class IntPoint
+    {
+        public final int x;
+        public final int y;
+
+        public IntPoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
