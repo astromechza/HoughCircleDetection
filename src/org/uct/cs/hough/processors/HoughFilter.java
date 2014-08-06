97b59035df74b703eb7f1f15405660235ee7edca
Ben Meier <benmeier42@gmail.com>
2014-08-06 15:17:02 +0200

    Moved to explicit midpoint circle algorithm in hough filter

--- src/org/uct/cs/hough/processors/HoughFilter.java
+++ src/org/uct/cs/hough/processors/HoughFilter.java
@@ -1,5 +1,6 @@
 package org.uct.cs.hough.processors;
 
+import org.uct.cs.hough.CircleDetection;
 import org.uct.cs.hough.reader.ShortImageBuffer;
 import org.uct.cs.hough.util.Circle;
 import org.uct.cs.hough.util.CircumferenceProvider;
@@ -11,48 +12,89 @@ public class HoughFilter
 {
     public static List<Circle> identify(ShortImageBuffer edges, float centerThreshold)
     {
+        int border = CircleDetection.MAX_RADIUS;
         int height = edges.getHeight();
+        int heightWithBorder = height + 2*border;
         int width = edges.getWidth();
+        int widthWithBorder = width + 2*border;
         int depth = CircumferenceProvider.getPointlists().size();
 
         // create hough space
-        int[][][] space = new int[height][width][depth];
-
+        int[][][] space = new int[heightWithBorder][widthWithBorder][depth];
+        int nx,ny,ay,ax,cx,cy,radiusError;
         for(int y=0;y<height;y++)
         {
+            ay = y + CircleDetection.MAX_RADIUS;
             for(int x=0;x<width;x++)
             {
+                ax = x + CircleDetection.MAX_RADIUS;
                 if (edges.get(y,x) != 0)
                 {
-                    int r = 0;
-                    for(int[][] pointlist : CircumferenceProvider.getPointlists())
+                    for(int r=0;r<depth;r++)
                     {
-                        for(int[] p : pointlist)
+                        cx = CircleDetection.MIN_RADIUS + r;
+                        cy = 0;
+                        radiusError = 1-cx;
+                        while(cx >= cy)
                         {
-                            int nx = x + p[0];
-                            int ny = y + p[1];
-                            if (nx >= 0 && nx < width && ny >= 0 && ny < height ) space[ny][nx][r]++;
+                            nx = ax + cx;
+                            ny = ay + cy;
+                            space[ny][nx][r]++;
+                            nx = ax + cy;
+                            ny = ay + cx;
+                            space[ny][nx][r]++;
+                            nx = ax - cx;
+                            ny = ay + cy;
+                            space[ny][nx][r]++;
+                            nx = ax - cy;
+                            ny = ay + cx;
+                            space[ny][nx][r]++;
+                            nx = ax - cx;
+                            ny = ay - cy;
+                            space[ny][nx][r]++;
+                            nx = ax - cy;
+                            ny = ay - cx;
+                            space[ny][nx][r]++;
+                            nx = ax + cx;
+                            ny = ay - cy;
+                            space[ny][nx][r]++;
+                            nx = ax + cy;
+                            ny = ay - cx;
+                            space[ny][nx][r]++;
+
+                            cy++;
+                            if (radiusError<0)
+                            {
+                                radiusError += 2 * cy + 1;
+                            }
+                            else
+                            {
+                                cx--;
+                                radiusError += 2 * (cy - cx + 1);
+                            }
                         }
-                        r++;
                     }
                 }
             }
         }
 
+        // cache circumference lenths
+        float[] cl = new float[depth];
+        for(int r=0;r<depth;r++) cl[r] = CircumferenceProvider.get(CircleDetection.MIN_RADIUS + r).length;
+
         List<Circle> output = new ArrayList<>();
         for(int y=0;y<height;y++)
         {
+            ay = y + CircleDetection.MAX_RADIUS;
             for(int x=0;x<width;x++)
             {
-                int r = 0;
-                for (int[][] pointlist : CircumferenceProvider.getPointlists())
+                ax = x + CircleDetection.MAX_RADIUS;
+                for(int r=0;r<depth;r++)
                 {
-                    float score = ((float)(space[y][x][r])) / pointlist.length;
-                    if (score > centerThreshold)
+                    if ((space[ay][ax][r] / cl[r]) > centerThreshold)
                     {
-                        output.add(new Circle(x,y,CircumferenceProvider.getMinRadius() + r));
+                        output.add(new Circle(x,y,CircleDetection.MIN_RADIUS + r));
                     }
-                    r++;
                 }
             }
         }
